cd7099e06a4337b66aa2f6e6083430a09af39b59
howyi <qrbys@outlook.com>
2018-03-06 14:57:15 +0900

    clean

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('user/{name}', function ($name) {
-	return $name;
-})->where('name', '[A-Za-z]+');
